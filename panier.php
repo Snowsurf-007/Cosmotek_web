4dec80c1cfca7e3564cc4524aea015c5f614b7cd
Lucien LEHEUDRE--EPSTEIN <lucien.leheudre--epstein@etu.cyu.fr>
2026-03-23 13:26:49 +0100

    Add HTML structure to panier.php
    Added HTML structure to panier.php including head and body.

---
+++ panier.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Panier</title>
+    <link href="Photos/Logo.png" alt="Logo planete" rel="icon">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="fichier.css" media="screen"/>
+</head>
+<body>
+</body>
+</html>
