cce2f8db95e590ef38e2b220307c3d5f33bd1cd5
Lucien LEHEUDRE--EPSTEIN <lucien.leheudre--epstein@etu.cyu.fr>
2026-03-23 13:44:41 +0100

    Add header.php with site navigation and logo

---
+++ header.php
@@ -0,0 +1,16 @@
+<header>
+    <div class="header-container">
+        <a href="accueil.php" class="logo-link">
+            <img src="Photos/Logo.png" alt="Cosmotek Logo" class="header-logo">
+            <span class="site-name">Cosmotek</span>
+        </a>
+        <nav class="header-nav">
+            <a href="carte.php">CARTE</a>
+            <a href="inscription.php">INSCRIPTION</a>
+            <a href="connexion.php">CONNEXION</a>
+            <a href="profil.php">PROFIL</a>
+            <a href="avis.php">AVIS</a>
+            <a href="panier.php"></a>
+        </nav>
+    </div>
+</header>
