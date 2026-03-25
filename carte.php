caec68e9727b0367c750bfbefd4641b8f9016d4f
Lucien LEHEUDRE--EPSTEIN <lucien.leheudre--epstein@etu.cyu.fr>
2026-03-25 11:06:13 +0100

    Remove footer HTML and include footer.php
    Removed the footer section from the carte.php file and included footer.php instead.

--- carte.php
+++ carte.php
@@ -121,24 +121,10 @@ $boutons_filtres = [
         <br><br>
         <a href="#top">RETOUR HAUT DE PAGE</a>
     </div>
+ 
+<?php
+    include ("footer.php");
+?>
 
-    <footer>
-        <div class="footer-container">
-            <div class="footer-section">
-                <h4>À PROPOS</h4>
-                <p>Cosmotek - Restaurant intergalactique depuis 2026</p>
-            </div>
-            <div class="footer-section">
-                <h4>NAVIGATION</h4>
-                <ul class="footer-links">
-                    <li><a href="accueil.html">Accueil</a></li>
-                    <li><a href="carte.html">Notre Carte</a></li>
-                </ul>
-            </div>
-            </div>
-        <div class="footer-bottom">
-            <p>&copy; 2026 Cosmotek - Tous droits réservés</p>
-        </div>
-    </footer>
 </body>
 </html>
