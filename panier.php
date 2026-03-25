cba2c1dd43f98f61864efdbb130c5e8fc793fa78
Lucien LEHEUDRE--EPSTEIN <lucien.leheudre--epstein@etu.cyu.fr>
2026-03-25 11:08:56 +0100

    Fix PHP tags for header and footer includes

--- panier.php
+++ panier.php
@@ -9,9 +9,15 @@
 </head>
 <body>
  
-    <?php
-        include ("header.php");
-    ?>
+<?php
+    include ("header.php");
+?>
+
+
+    
+<?php
+    include ("footer.php");
+?>
     
 </body>
 </html>
