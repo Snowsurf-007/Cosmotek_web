75880ea4388c47780a441f8ed00315f3e5d065ee
ibratra94 <ibrahima.traore4@etu.cyu.fr>
2026-04-03 22:19:30 +0200

    Update connexion.php

--- connexion.php
+++ connexion.php
@@ -1,45 +1,6 @@
 <?php
 session_start();
 ?>
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Connexion</title>
-    <link href="Photos/Logo.png" alt="Logo planete" rel="icon">
-    <link rel="stylesheet" href="fichier.css" media="screen"/>
-</head>
-<body style="display: flex; flex-direction: column;">
-  <?php
-    include ("header.php");
-  ?>
-	
-	<div class="page">
-    <h2>Connexion</h2>
-
-    <form method="post" action="connexion.php">
-        <div class="connexion">
-            <label></label>
-            <input type="email" name="email" id="email" placeholder="Email*">
-        </div>
-		<br>
-        <div class="connexion">
-            <label></label>
-            <input type="password" name="mdp" id="mdp" placeholder="Mot de pase*">
-        </div>
-		<br>
-        <input type="submit" value="Se connecter" class="bouton">
-    </form>
-</div>
-
-<?php
-    include ("footer.php");
-?>
-
-</body>
-</html>
-
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST'){
 
@@ -87,17 +48,19 @@ foreach($admin as $control){
                 header("Location: profil.php");
                 exit();
             }
-            elseif($uber['email'] === $email && password_verify($mdp, $uber['mdp'])){
-                $_SESSION['email'] = $uber['email'];
-                $_SESSION['nom'] = $uber['nom'];
-                $_SESSION['date_inscription'] = $uber['date_inscription'];
-                $_SESSION['statut'] = $uber['statut'];
-                $_SESSION['prenom'] = $uber['prenom'];
-                $_SESSION['adresse'] = $uber['adresse'];
-                $_SESSION['derniere'] = date("d/m/Y");
-                header("Location: livraison.php");
-                exit();
-            }
+            else{
+                if($uber['email'] === $email && password_verify($mdp, $uber['mdp'])){
+                    $_SESSION['email'] = $uber['email'];
+                    $_SESSION['nom'] = $uber['nom'];
+                    $_SESSION['date_inscription'] = $uber['date_inscription'];
+                    $_SESSION['statut'] = $uber['statut'];
+                    $_SESSION['prenom'] = $uber['prenom'];
+                    $_SESSION['adresse'] = $uber['adresse'];
+                    $_SESSION['derniere'] = date("d/m/Y");
+                    header("Location: livraison.php");
+                    exit();
+                }
+            }   
         }    
     }
 }
@@ -266,3 +229,41 @@ foreach($utilisateurs as $user){
 echo "<script>alert('Email ou mot de passe incorrect');</script>";
 }
 ?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Connexion</title>
+    <link href="Photos/Logo.png" alt="Logo planete" rel="icon">
+    <link rel="stylesheet" href="fichier.css" media="screen"/>
+</head>
+<body style="display: flex; flex-direction: column;">
+  <?php
+    include ("header.php");
+  ?>
+	
+	<div class="page">
+    <h2>Connexion</h2>
+
+    <form method="post" action="connexion.php">
+        <div class="connexion">
+            <label></label>
+            <input type="email" name="email" id="email" placeholder="Email*">
+        </div>
+		<br>
+        <div class="connexion">
+            <label></label>
+            <input type="password" name="mdp" id="mdp" placeholder="Mot de pase*">
+        </div>
+		<br>
+        <input type="submit" value="Se connecter" class="bouton">
+    </form>
+</div>
+
+<?php
+    include ("footer.php");
+?>
+
+</body>
+</html>
