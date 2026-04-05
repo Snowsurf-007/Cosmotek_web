3667ae92d8cb16e1fa178aca77742cd3d8ddf6a7
Bibi110 <hugo.treny@etu.cyu.fr>
2026-04-05 23:20:59 +0200

    Update header links to use relative paths

--- back/header2.php
+++ back/header2.php
@@ -1,13 +1,13 @@
 <header>
     <div class="header-container">
-        <a href="index.php" class="logo-link">
+        <a href="../index.php" class="logo-link">
             <span class="site-name">&nbsp;</span>
-            <img src="Photos/Logo.png" alt="Cosmotek Logo" class="header-logo">
+            <img src="../Photos/Logo.png" alt="Cosmotek Logo" class="header-logo">
             <span class="site-name">Cosmotek&nbsp;</span>
         </a>
         <nav class="header-nav">
-            <a href="connexion.php">CONNEXION</a>
-            <a href="profil.php">PROFIL</a>
+            <a href="../client/connexion.php">CONNEXION</a>
+            <a href="../client/profil.php">PROFIL</a>
         </nav>
     </div>
 </header>
