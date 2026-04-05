ddd219652f46a51e7a53f0651b3f45c93cabe17e
Lucien LEHEUDRE--EPSTEIN <lucien.leheudre--epstein@etu.cyu.fr>
2026-04-05 12:10:39 +0200

    Add panier functionality to handle items in session

---
+++ ajout_panier.php
@@ -0,0 +1,48 @@
+<?php
+session_start();
+
+if (isset($_POST['nom'])) {
+    $nom = $_POST['nom'];
+} else {
+    $nom = '';
+}
+
+if (isset($_POST['prix'])) {
+    $prix = $_POST['prix'];
+} else {
+    $prix = '';
+}
+
+if (isset($_POST['image'])) {
+    $image = $_POST['image'];
+} else {
+    $image = '';
+}
+
+if (!empty($nom)) {
+    if (!isset($_SESSION['panier'])) {
+        $_SESSION['panier'] = [];
+    }
+
+    $trouve = false;
+  
+    foreach ($_SESSION['panier'] as &$item) {
+        if ($item['nom'] === $nom) {
+            $item['quantite']++;
+            $trouve = true;
+            break;
+        }
+    }
+    unset($item);
+
+    if (!$trouve) {
+        $_SESSION['panier'][] = [
+            'nom'      => $nom,
+            'prix'     => (float) $prix,
+            'image'    => $image,
+            'quantite' => 1
+        ];
+    }
+}
+
+exit;
