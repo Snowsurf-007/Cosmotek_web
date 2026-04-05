5beb0d04320c35c428e696dfdf0c48b5f301c4d7
ibratra94 <ibrahima.traore4@etu.cyu.fr>
2026-04-05 10:41:08 +0200

    Add admin session check in admin.php

--- admin.php
+++ admin.php
@@ -1,6 +1,9 @@
 <?php
 session_start();
-
+if (!isset($_SESSION['statut']) || $_SESSION['statut'] !== 'admin') {
+    header("Location: connexion.php");
+    exit();
+}
 $fichier = "users.json";
 $utilisateurs = [];
 function toto($str1) {
