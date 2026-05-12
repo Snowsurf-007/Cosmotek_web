62aeeef52dbe256bc812cb1afc3ffd72b4017b79
ibratra94 <ibrahima.traore4@etu.cyu.fr>
2026-05-12 12:19:32 +0200

    Add statut.php to retrieve user status

---
+++ statut.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+$fichier = "users.json";
+
+// On récupère l'ID de la personne a la session
+$id = $_SESSION['user_id'] ?? null;
+
+if ($id !== null && file_exists($fichier)) {
+    $utilisateurs = json_decode(file_get_contents($fichier), true);
+    
+    if (isset($utilisateurs[$id])) {
+        // On renvoie le statut en json au javascript
+        echo json_encode(['statut' => $utilisateurs[$id]['statut']]);
+        exit;
+    }
+}
+echo json_encode(['statut' => 'actif']);
+?>
