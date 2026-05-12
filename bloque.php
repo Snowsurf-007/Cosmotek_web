f9d94f6a593c6a46b5a70343d595b8650b46be9e
ibratra94 <ibrahima.traore4@etu.cyu.fr>
2026-05-12 15:52:12 +0200

    Create bloque.php

---
+++ bloque.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bloque - Cosmotek</title>
+    <link href="Photos/Logo.png" rel="icon">
+    <link rel="stylesheet" href="fichier.css" media="screen"/>
+</head>
+<body style="display: flex; flex-direction: column;">
+
+    <?php 
+        include("header.php");
+    ?>
+    <div class="page">
+        <h1> BLOQUE </h1>
+    </div>
+
+    <?php
+        include("footer.php"); 
+    ?>
+
+</body>
+</html>
