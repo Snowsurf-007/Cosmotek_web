4d79dc6b628607eb85b459998d9356340a9951e1
ibratra94 <ibrahima.traore4@etu.cyu.fr>
2026-05-16 03:42:14 +0200

    Refactor session status retrieval logic in statut.php

--- statut.php
+++ statut.php
@@ -1,17 +1,30 @@
 <?php
 session_start();
+
 $fichier = "users.json";
 
-// On récupère l'ID de la personne a la session
+// recup id 
 $id = $_SESSION['user_id'] ?? null;
 
-if ($id !== null && file_exists($fichier)) {
+// pas co rien
+if ($id === null) {
+    echo json_encode(['statut' => 'visiteur']);
+    exit();
+}
+
+// 2. On va lire le fichier JSON frais sur le serveur pour voir les modifs de l'admin
+if (file_exists($fichier)) {
     $utilisateurs = json_decode(file_get_contents($fichier), true);
-    
     if (isset($utilisateurs[$id])) {
-        // On renvoie le statut en json au javascript
+        if ($utilisateurs[$id]['statut'] === 'bloque') {
+            session_unset();
+            session_destroy();
+        }
+        //actu en direct
         echo json_encode(['statut' => $utilisateurs[$id]['statut']]);
-        exit;
+        exit();
     }
 }
+
+echo json_encode(['statut' => 'inconnu']);
 ?>
