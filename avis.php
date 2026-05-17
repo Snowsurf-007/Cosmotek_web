7188e965f08859f23cec227bc0cb106ad50b0770
ibratra94 <ibrahima.traore4@etu.cyu.fr>
2026-05-17 21:56:02 +0200

    Update avis.php

--- avis.php
+++ avis.php
@@ -72,6 +72,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         return true;
     }
     </script>
+    <script src="verifstat.js" defer></script>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Avis - Cosmotek</title>
