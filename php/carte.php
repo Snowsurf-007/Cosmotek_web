5d9ef9676ecc626399893b504acc3833ed2faf06
Lucien LEHEUDRE--EPSTEIN <lucien.leheudre--epstein@etu.cyu.fr>
2026-05-31 17:58:57 +0200

    Update carte.php

--- php/carte.php
+++ php/carte.php
@@ -20,6 +20,7 @@ $boutons_filtres = [
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cosmotek - Carte</title>
     <link rel="stylesheet" href="../css/style.css">
     <script src="../js/carte.js" defer></script>
